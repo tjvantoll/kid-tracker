e85e7e6cc4d8f4501a5ae9f0e0c9b8dd6c316ff1
TJ VanToll <tj.vantoll@gmail.com>
2022-04-27 15:15:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kid Tracker
+
+> **WARNING**: 🚧 This repository is a work in progress 🚧
+
+This repo contains the source code for a custom GPS tracker for kids. For full details on how to use this code, see the [project’s full writeup on Hackster]().
---
+++ sketch.ino
@@ -0,0 +1,86 @@
+#include <Notecard.h>
+
+#define serialDebug Serial
+#define productUID "YOUR_VALUE_HERE"
+#define BUTTON_PIN USER_BTN
+#define SMS_FROM "YOUR_VALUE_HERE"
+#define SMS_TO "YOUR_VALUE_HERE"
+
+Notecard notecard;
+bool locationRequested = false;
+
+void ISR(void) {
+  serialDebug.println("Button pressed");
+  locationRequested = true;
+}
+
+void setup() {
+  serialDebug.begin(115200);
+
+  notecard.begin();
+  notecard.setDebugOutputStream(serialDebug);
+ 
+  J *req1 = notecard.newRequest("hub.set");
+  JAddStringToObject(req1, "product", productUID);
+  JAddStringToObject(req1, "mode", "periodic");
+  JAddNumberToObject(req1, "outbound", 5);
+  notecard.sendRequest(req1);
+
+  J *req2 = notecard.newRequest("card.location.mode");
+  JAddStringToObject(req2, "mode", "periodic");
+  JAddNumberToObject(req2, "seconds", 60);
+  notecard.sendRequest(req2);
+
+  J *req3 = notecard.newRequest("card.location.track");
+  JAddBoolToObject(req3, "start", true);
+  JAddBoolToObject(req3, "heartbeat", true);
+  JAddNumberToObject(req3, "hours", 12);
+  notecard.sendRequest(req3);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), ISR, FALLING);
+
+  serialDebug.println("Setup complete");
+}
+
+void loop() {
+  if (locationRequested) {
+    J *req = notecard.newRequest("card.location");
+    J *resp = notecard.requestAndResponse(req);
+    serialDebug.println("Location:");
+    double lat = JGetNumber(resp, "lat");
+    double lon = JGetNumber(resp, "lon");
+    serialDebug.println(lat, 10);
+    serialDebug.println(lon, 10);
+
+    if (lat == 0) {
+      serialDebug.println("The Notecard does not yet have a location");
+      // Wait a minute before trying again.
+      delay(1000 * 60);
+    } else {
+      // http://maps.google.com/maps?q=<lat>,<lon>
+      char buffer[100];
+      snprintf(
+        buffer,
+        sizeof(buffer),
+        "Your kids are requesting you. https://maps.google.com/maps?q=%.8lf,%.8lf",
+        lat,
+        lon
+       );
+      serialDebug.println(buffer);
+
+      J *req2 = notecard.newRequest("note.add");
+      JAddStringToObject(req2, "file", "twilio.qo");
+      JAddBoolToObject(req2, "sync", true);
+      J *body = JCreateObject();
+      JAddStringToObject(body, "body", buffer);
+      JAddStringToObject(body, "from", SMS_FROM);
+      JAddStringToObject(body, "to", SMS_TO);
+      JAddItemToObject(req2, "body", body);
+      notecard.sendRequest(req2);
+  
+      locationRequested = false;
+      serialDebug.println("Location sent successfully.");
+    }
+  }
+}
